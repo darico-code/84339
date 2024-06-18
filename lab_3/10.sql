662dedb3f127bc40c966fcda4db37a1138440576
Dario <dariobekic91@gmail.com>
2024-06-18 15:27:41 +0200

    fix old version

--- lab_3/10.sql
+++ lab_3/10.sql
@@ -4,18 +4,18 @@
 
 SET SERVEROUTPUT ON; 
 
-CREATE OR REPLACE PROCEDURE showClient(orderId number) IS 
+CREATE OR REPLACE PROCEDURE showClient(orderIdPar number) IS 
 clientName NW_CUSTOMERS.contactname%TYPE;
 BEGIN 
     SELECT c.contactname INTO clientName
-    FROM NW_CUSTOMERS c JOIN NW_ORDERS o ON c.customerid = o.customerid WHERE o.orderId = orderId;
+    FROM NW_CUSTOMERS c JOIN NW_ORDERS o ON c.customerid = o.customerid WHERE o.orderId = orderIdPar;
     DBMS_OUTPUT.PUT_LINE(clientName);
     EXCEPTION 
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('Client not found');
-END visualizza_cliente;
+END showClient;
 /
 
 BEGIN
-    visualizza_cliente(22);
+    showClient(10436);
 END;
